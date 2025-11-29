a6e12e8f6c5f4dc4627ae4f91f240247d0e36b01
chpsdsh.fedora <bmxsib1@gmail.com>
2025-11-29 17:48:30 +0700

    lab-1.6: added reaper for detach

--- lab-1.6/main.c
+++ lab-1.6/main.c
@@ -10,9 +10,32 @@ static void* worker(void* p) {
     return (void*)(id * 2);
 }
 
+
+
+static void* worker_finite(void* p) {
+    uintptr_t id = (uintptr_t)p;
+    printf("[finite] thread %lu: start\n", (unsigned long)id);
+    usleep(500000);
+    printf("[finite] thread %lu: done\n", (unsigned long)id);
+    return (void*)(id * 2);
+}
+
+static void* worker_infinite(void* p) {
+    uintptr_t id = (uintptr_t)p;
+    printf("[infinite] thread %lu: start \n", (unsigned long)id);
+
+    while (1) {
+        usleep(100000); 
+    }
+
+    return NULL;
+}
+
 void demo_join_all(void) {
     const int N = 8;
     mythread_t th[N];
+
+    printf("=== demo_join_all: creating %d threads ===\n", N);
 
     for (int i = 0; i < N; ++i) {
         mythread_create(&th[i], worker, (void*)(uintptr_t)i);
@@ -21,21 +44,38 @@ void demo_join_all(void) {
     for (int i = 0; i < N; ++i) {
         void* ret = NULL;
         mythread_join(th[i], &ret);
-        printf("thread %d -> retval=%lu\n", i, (unsigned long)(uintptr_t)ret);
+        printf("thread %d -> retval=%lu\n",
+               i, (unsigned long)(uintptr_t)ret);
     }
+
+    printf("=== demo_join_all: done ===\n\n");
 }
 
+void demo_detach_two(void) {
+    mythread_t th_finite, th_infinite;
 
-void demo_detach_one(void) {
-    mythread_t t;
-    mythread_create(&t, worker, (void*)(uintptr_t)23);
-    mythread_detach(t);           
-    usleep(20000);                
-    printf("detached thread finished silently\n");
+    printf("=== demo_detach_two: create finite + infinite threads ===\n");
+
+    mythread_create(&th_finite,   worker_finite,   (void*)(uintptr_t)1);
+    mythread_create(&th_infinite, worker_infinite, (void*)(uintptr_t)2);
+
+    printf("detaching finite thread...\n");
+    mythread_detach(th_finite);
+    printf("finite thread detached\n");
+
+    printf("detaching infinite thread...\n");
+    mythread_detach(th_infinite);
+    printf("detaching infinite thread detached\n");
+    printf("=== demo_detach_two: done  ===\n");
 }
 
 int main(void) {
+    printf("===== TEST: JOIN =====\n");
     demo_join_all();
-    demo_detach_one();
+
+    printf("===== TEST: DETACH =====\n");
+    demo_detach_two();
+    usleep(2000000);  
+    printf("main: finished\n");
     return 0;
 }
--- lab-1.6/my_thread.c
+++ lab-1.6/my_thread.c
@@ -1,5 +1,77 @@
 #include "my_thread.h"
 
+static _Atomic int reaper_started = 0;
+static _Atomic int reaper_futex = 0;
+static _Atomic(mythread *) reaper_list = NULL;
+
+static void reaper_enqueue(mythread *t)
+{
+    mythread *old_head;
+    while (1)
+    {
+        old_head = atomic_load(&reaper_list);
+        t->next_reap = old_head;
+        if (atomic_compare_exchange_weak(&reaper_list, &old_head, t))
+        {
+            break;
+        }
+    }
+}
+
+static int reaper_func(void *arg)
+{
+    (void)arg;
+
+    for (;;)
+    {
+        while (atomic_load(&reaper_futex) == 0)
+        {
+            futex_wait(&reaper_futex, 0);
+        }
+
+        atomic_store(&reaper_futex, 0);
+
+        mythread *list = atomic_exchange(&reaper_list, NULL);
+        while (list)
+        {
+            mythread *next = list->next_reap;
+            release_thread(list);
+            list = next;
+        }
+    }
+    return 0;
+}
+
+static void start_reaper_if_needed(void)
+{
+    int expected = 0;
+    if (!atomic_compare_exchange_strong(&reaper_started, &expected, 1))
+        return;
+
+    void *stack = mmap(NULL, MYTHREAD_STACK_SIZE,
+                       PROT_READ | PROT_WRITE,
+                       MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK,
+                       -1, 0);
+    if (stack == MAP_FAILED)
+    {
+        atomic_store(&reaper_started, 0);
+        return;
+    }
+
+    void *stack_top = (uint8_t *)stack + MYTHREAD_STACK_SIZE;
+
+    int flags = CLONE_VM | CLONE_FS | CLONE_FILES |
+                CLONE_SIGHAND | CLONE_SYSVSEM | CLONE_THREAD;
+
+    int rc = clone(reaper_func, stack_top, flags, NULL);
+    if (rc == -1)
+    {
+        munmap(stack, MYTHREAD_STACK_SIZE);
+        atomic_store(&reaper_started, 0);
+        return;
+    }
+}
+
 static int trampoline_func(void *arg)
 {
     mythread *t = (mythread *)arg;
@@ -11,10 +83,16 @@ static int trampoline_func(void *arg)
     }
 
     t->retval = ret;
+
     atomic_store(&t->futex_word, 1);
     futex_wake(&t->futex_word, 1);
 
-    atomic_fetch_sub(&t->refs, 1);
+    if (atomic_load(&t->joinable) == 0)
+    {
+        reaper_enqueue(t);
+        atomic_store(&reaper_futex, 1);
+        futex_wake(&reaper_futex, 1);
+    }
 
     syscall(SYS_exit, 0);
     return 0;
@@ -29,6 +107,8 @@ int mythread_create(mythread_t *thread,
         return MYTHREAD_EINVAL;
     }
 
+    start_reaper_if_needed();
+
     mythread *t = (mythread *)calloc(1, sizeof(*t));
     if (!t)
         return MYTHREAD_ERR;
@@ -38,8 +118,8 @@ int mythread_create(mythread_t *thread,
     t->retval = NULL;
 
     atomic_store(&t->joinable, 1);
-    atomic_store(&t->refs, 2);
     atomic_store(&t->futex_word, 0);
+    t->next_reap = NULL;
 
     t->stack_size = MYTHREAD_STACK_SIZE;
 
@@ -78,45 +158,42 @@ int mythread_join(mythread_t thread, void **retval)
         return MYTHREAD_EINVAL;
     }
 
-    if (atomic_load(&thread->joinable) == 0)
+    int expected = 1;
+    if (!atomic_compare_exchange_strong(&thread->joinable, &expected, -1))
     {
         return MYTHREAD_ESTATE;
     }
 
-    for (;;)
+    while (atomic_load(&thread->futex_word) == 0)
     {
-        int f = atomic_load(&thread->futex_word);
-        if (f == 1)
-            break;
         futex_wait(&thread->futex_word, 0);
     }
 
     if (retval)
         *retval = thread->retval;
 
-    int old = atomic_fetch_sub(&thread->refs, 1);
-    if (old == 1)
-    {
-        release_thread(thread);
-    }
+    release_thread(thread);
+
     return MYTHREAD_OK;
 }
 
-int mythread_detach(mythread_t thread) {
-    if (!thread) return MYTHREAD_EINVAL;
+int mythread_detach(mythread_t thread)
+{
+    if (!thread)
+        return MYTHREAD_EINVAL;
 
     int expected = 1;
-    if (!atomic_compare_exchange_strong(&thread->joinable, &expected, 0)) {
-        return MYTHREAD_ESTATE; 
+    if (!atomic_compare_exchange_strong(&thread->joinable, &expected, 0))
+    {
+        return MYTHREAD_ESTATE;
     }
 
-    while (atomic_load(&thread->futex_word) == 0) {
-        futex_wait(&thread->futex_word, 0);
+    if (atomic_load(&thread->futex_word) == 1)
+    {
+        reaper_enqueue(thread);
+        atomic_store(&reaper_futex, 1);
+        futex_wake(&reaper_futex, 1);
     }
 
-    int old = atomic_fetch_sub(&thread->refs, 1);
-    if (old == 1) {
-        release_thread(thread);
-    }
     return MYTHREAD_OK;
 }
